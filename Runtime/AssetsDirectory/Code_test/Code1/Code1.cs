826789123ccc407b072c33bd597642be4999620a
Yaroslav Tymoshenko <jarzykk@gmail.com>
2025-09-25 17:20:33 +0300

    Update test files with new assets

---
+++ Runtime/AssetsDirectory/Code_test/Code1/Code1.cs
@@ -0,0 +1,7 @@
+namespace TestCore.Code.Test.Runtime.AssetsDirectory.Code_test.Code1
+{
+    public class Code1
+    {
+        // It's a test'
+    }
+}
---
+++ Runtime/AssetsDirectory/Code_test/Code2/Code2.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+namespace TestCore.Code.Test.Runtime.AssetsDirectory.Code_test.Code2
+{
+    public class Code2 : MonoBehaviour
+    {
+        private void Start() => 
+            Debug.Log("Hello from Code2!");
+    }
+}
